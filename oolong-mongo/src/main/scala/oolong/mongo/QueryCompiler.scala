05ee05f120416557c3e09519787e97becead46fe
denis_savitsky <d.e.savitskiy@gmail.com>
2023-09-30 17:16:58 +0200

    Add renderQuery and updateQuery methods for test

--- oolong-mongo/src/main/scala/oolong/mongo/QueryCompiler.scala
+++ oolong-mongo/src/main/scala/oolong/mongo/QueryCompiler.scala
@@ -8,6 +8,7 @@ import org.mongodb.scala.bson.BsonDocument
 
 /**
  * Compile a BSON description of the update.
+ *
  * @param input
  *   Description of the update written in oolong DSL.
  */
@@ -26,29 +27,40 @@ private[oolong] def updateImpl[Doc: Type](
   import quotes.reflect.*
   import MongoUpdateCompiler.*
 
-  val parser = new DefaultAstParser
-
-  val ast = parser.parseUExpr(input)
-
-  val optRepr   = opt(ast)
-  val optimized = optimize(optRepr)
+  val (ast, optimized) = buildUpdateAst(input)
 
   report.info("AST:\n" + pprint(ast) + "\nGenerated Mongo query:\n" + render(optimized))
 
   target(optimized)
 }
 
-private[oolong] def queryImpl[Doc: Type](input: Expr[Doc => Boolean])(using quotes: Quotes): Expr[BsonDocument] = {
-  import quotes.reflect.*
-  import MongoQueryCompiler.*
+private[oolong] def buildUpdateAst[Doc: Type](input: Expr[Updater[Doc] => Updater[Doc]])(using
+    quotes: Quotes
+): (UExpr, MongoUpdateNode) =
+  import MongoUpdateCompiler.*
 
+  val parser = new DefaultAstParser
+
+  val ast = parser.parseUExpr(input)
+
+  val optRepr = opt(ast)
+  ast -> optimize(optRepr)
+
+private[oolong] def buildQueryAst[Doc: Type](input: Expr[Doc => Boolean])(using quotes: Quotes): (QExpr, MongoQueryNode) =
+  import MongoQueryCompiler.*
   val parser = new DefaultAstParser
 
   val ast          = parser.parseQExpr(input)
   val optimizedAst = LogicalOptimizer.optimize(ast)
 
-  val optRepr   = opt[Doc](optimizedAst)
-  val optimized = optimize(optRepr)
+  val optRepr = opt(optimizedAst)
+  optimizedAst -> optimize(optRepr)
+
+private[oolong] def queryImpl[Doc: Type](input: Expr[Doc => Boolean])(using quotes: Quotes): Expr[BsonDocument] = {
+  import quotes.reflect.*
+  import MongoQueryCompiler.*
+
+  val (optimizedAst, optimized) = buildQueryAst(input)
 
   report.info("Optimized AST:\n" + pprint(optimizedAst) + "\nGenerated Mongo query:\n" + render(optimized))
 
---
+++ oolong-mongo/src/test/scala/oolong/mongo/render.scala
@@ -0,0 +1,27 @@
+package oolong.mongo
+
+import scala.quoted.*
+
+import oolong.dsl.Updater
+
+inline def renderUpdate[Doc](inline input: Updater[Doc] => Updater[Doc]): String =
+  ${ renderUpdateImpl('input) }
+
+def renderUpdateImpl[Doc: Type](
+    input: Expr[Updater[Doc] => Updater[Doc]]
+)(using quotes: Quotes) =
+  import MongoUpdateCompiler.*
+  val (_, query) = buildUpdateAst(input)
+  val result         = render(query)
+  Expr(result)
+
+inline def renderQuery[Doc](inline input: Doc => Boolean): String =
+  ${ renderQueryImpl('input) }
+
+def renderQueryImpl[Doc: Type](
+    input: Expr[Doc => Boolean]
+)(using quotes: Quotes) =
+  import MongoQueryCompiler.*
+  val (_, query) = buildQueryAst(input)
+  val result         = render(query)
+  Expr(result)

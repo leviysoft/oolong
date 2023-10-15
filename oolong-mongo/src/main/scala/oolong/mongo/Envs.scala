93cbd4a5d5c321c39984c41d293c7dd5451b9f55
denis_savitsky <d.e.savitskiy@gmail.com>
2023-10-15 22:30:50 +0200

    Remove `extends App`

--- oolong-mongo/src/main/scala/oolong/mongo/Envs.scala
+++ oolong-mongo/src/main/scala/oolong/mongo/Envs.scala
@@ -1,6 +1,6 @@
 package oolong.mongo
 
-object Envs extends App:
+object Envs:
   private val printAstKey = "oolong.print.ast"
   private[oolong] lazy val printAst =
     Option(System.getProperty(printAstKey)).orElse(sys.env.get(printAstKey)).getOrElse("false").toBoolean

6b94e7bc8b449e2307abadb8053f12f1d29f2163
Daniel Slapman <danslapman@gmail.com>
2024-01-02 21:29:20 +0100

    Simplify refined instances

--- oolong-bson-refined/src/main/scala/oolong/bson/refined/refined.scala
+++ oolong-bson-refined/src/main/scala/oolong/bson/refined/refined.scala
@@ -24,7 +24,7 @@ given [T, P, F[_, _]](using BsonDecoder[T], Validate[T, P], RefType[F]): BsonDec
       case Failure(exc) => Failure(exc)
     }
 
-given [T, P, F[_, _]](using BsonEncoder[T], Validate[T, P], RefType[F]): BsonEncoder[F[T, P]] with
+given [T, P, F[_, _]](using BsonEncoder[T], RefType[F]): BsonEncoder[F[T, P]] with
   extension (value: F[T, P]) def bson: BsonValue = RefType[F].unwrap(value).bson
 
 given [T, P, F[_, _]](using BsonKeyDecoder[T], Validate[T, P], RefType[F]): BsonKeyDecoder[F[T, P]] with
@@ -38,5 +38,5 @@ given [T, P, F[_, _]](using BsonKeyDecoder[T], Validate[T, P], RefType[F]): Bson
       case Failure(exc) => Failure(exc)
     }
 
-given [T, P, F[_, _]](using BsonKeyEncoder[T], Validate[T, P], RefType[F]): BsonKeyEncoder[F[T, P]] with
+given [T, P, F[_, _]](using BsonKeyEncoder[T], RefType[F]): BsonKeyEncoder[F[T, P]] with
   def encode(t: F[T, P]): String = BsonKeyEncoder[T].encode(RefType[F].unwrap(t))
